7829dde29b792731765d0090fc78be8550f51129
Nick Johnstone <ncwjohnstone@gmail.com>
2014-06-24 11:32:59 +1200

    Added rails skeleton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsUrlShortener::Application.config.secret_token = 'd824c42ac4075cdec1e4552685239eccf67e608a4959c425c7a4abbfa7ed71dd1399aab0337db0edd19ad8abdcbcedf59f1b40a1d3593b1a104c3c4643a21f66'
