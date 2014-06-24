5fcb2fe71c81f1ed0da4bf26e47afe74bdf3ad37
Nick Johnstone <ncwjohnstone@gmail.com>
2014-06-24 17:30:57 +1200

    Added ShortUrlsController #create and #redirect

--- app/controllers/short_urls_controller.rb
+++ app/controllers/short_urls_controller.rb
@@ -1,2 +1,15 @@
 class ShortUrlsController < ApplicationController
+  respond_to :html
+
+  def create
+    short_url = ShortUrl.create(params[:short_url])
+
+    respond_with short_url, location: '/'
+  end
+
+  def redirect
+    shortened_url = ShortUrl.find_by_short_url(params[:short_url])
+
+    redirect_to shortened_url.original_url
+  end
 end
